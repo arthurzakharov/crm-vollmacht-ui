1acff0358a3b33aeb9deb642a4f58d349cefdcf6
arthur.zakharov <arthur.zakharov@legal.one>
2023-11-16 14:12:30 +0100

    version [0.0.51]

--- src/components/logos/logos.tsx
+++ src/components/logos/logos.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { FC } from "react";
 import iconTuvSrc from "../../assets/jpeg/tuv/tuv.jpeg";
 import iconTlsSrc from "../../assets/png/tls/tls.png";
 import "./logos.css";
 
-const Logos = () => (
+type Logo = "tuv" | "tls";
+
+interface Props {
+  show: Logo[];
+}
+
+const Logos: FC<Props> = (props) => (
   <div className="logos">
-    <div className="logos__tls">
-      <img alt="tls-icon" src={iconTlsSrc} className="logos__icon" />
-      <span className="logos__text">Höchste</span>
-      <span className="logos__text">Datensicherheit.</span>
-    </div>
-    <img alt="tuv-icon" src={iconTuvSrc} className="logos__tuv" />
+    {props.show.map((logo) => {
+      switch (logo) {
+        case "tuv":
+          return <img key={logo} alt="tuv-icon" src={iconTuvSrc} className="logos__tuv" />;
+        case "tls":
+          return (
+            <div key={logo} className="logos__tls">
+              <img alt="tls-icon" src={iconTlsSrc} className="logos__icon" />
+              <span className="logos__text">Höchste</span>
+              <span className="logos__text">Datensicherheit.</span>
+            </div>
+          );
+      }
+    })}
   </div>
 );
 
