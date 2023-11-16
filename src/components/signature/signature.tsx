995e83950a6ae32b9af3372ffdfe9a978bd71014
arthur.zakharov <arthur.zakharov@legal.one>
2023-11-16 10:41:35 +0100

    version [0.0.50]

--- src/components/signature/signature.tsx
+++ src/components/signature/signature.tsx
@@ -30,7 +30,6 @@ const Signature: FC<Props> = (props) => {
   const { width, height } = useWindowSize();
 
   useEffect(() => {
-    console.log("signature", !!props.fullName, !props.signature);
     if (!!props.fullName && !props.signature) {
       props.fetchSignature();
     }
