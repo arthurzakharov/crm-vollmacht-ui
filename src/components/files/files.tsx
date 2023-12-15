138ea157bca70e51d9abd20492c06e8d5b49976d
arthur.zakharov <arthur.zakharov@legal.one>
2023-12-15 17:13:38 +0100

    version [0.0.160]

--- src/components/files/files.tsx
+++ src/components/files/files.tsx
@@ -54,7 +54,7 @@ function Files(props: PropsWithChildren<Props>) {
         delay={150}
         animateOpacity
         easing="cubic-bezier(0.4, 0, 0.2, 1)"
-        height={gotLetter === "yes" || props.areFilesUploaded ? "auto" : 0}
+        height={gotLetter === "yes" || !props.areFilesUploaded ? "auto" : 0}
       >
         <div className={cn("files__upload", { "files__upload--single": props.areFilesUploaded })}>{props.children}</div>
       </AnimateHeight>
