c7d8bccc02f42a4d2f06bdee28e3a884198f656f
arthur.zakharov <arthur.zakharov@legal.one>
2024-05-07 13:13:18 +0200

    Update Header

--- src/stories/components/header.stories.tsx
+++ src/stories/components/header.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import { Header } from "../../components";
 import logoSrc from "../assets/logo.png";
 
@@ -9,8 +9,81 @@ const meta: Meta<typeof Header> = {
   component: Header,
 };
 
-export const Default: Story = {
-  name: "Header",
+export const WithPhone: Story = {
+  name: "With Phone",
+  args: {
+    logo: logoSrc,
+    tel: "030 / 20 898 12 11",
+    descriptions: undefined,
+  },
+};
+
+export const WithoutPhone: Story = {
+  name: "Without Phone",
+  args: {
+    logo: logoSrc,
+    tel: undefined,
+    descriptions: undefined,
+  },
+};
+
+export const WithoutDescriptions: Story = {
+  name: "Without Descriptions",
+  args: {
+    logo: logoSrc,
+    tel: undefined,
+    descriptions: undefined,
+  },
+};
+
+export const WithDescriptionSizeS: Story = {
+  name: "With Descriptions Size S",
+  args: {
+    logo: logoSrc,
+    tel: undefined,
+    descriptions: [
+      {
+        size: "s",
+        value: "Kostenlose Erstberatung",
+      },
+    ],
+  },
+};
+
+export const WithDescriptionSizeM: Story = {
+  name: "With Descriptions Size M",
+  args: {
+    logo: logoSrc,
+    tel: undefined,
+    descriptions: [
+      {
+        size: "m",
+        value: "Mo-Fr von 8:00 - 20:00",
+      },
+    ],
+  },
+};
+
+export const WithDescriptionAllSize: Story = {
+  name: "With Descriptions All Size",
+  args: {
+    logo: logoSrc,
+    tel: undefined,
+    descriptions: [
+      {
+        size: "m",
+        value: "Mo-Fr von 8:00 - 20:00",
+      },
+      {
+        size: "s",
+        value: "Kostenlose Erstberatung",
+      },
+    ],
+  },
+};
+
+export const WithAllItems: Story = {
+  name: "With All Items",
   args: {
     logo: logoSrc,
     tel: "030 / 20 898 12 11",
@@ -24,6 +97,15 @@ export const Default: Story = {
         value: "Kostenlose Erstberatung",
       },
     ],
+  },
+};
+
+export const DefaultValues: Story = {
+  name: "Default Values",
+  args: {
+    logo: logoSrc,
+    tel: undefined,
+    descriptions: undefined,
   },
 };
 
