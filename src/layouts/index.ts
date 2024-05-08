54a95da65d61283d4b1679b9fb174a6592cc12d1
arthur.zakharov <arthur.zakharov@legal.one>
2024-05-08 15:33:25 +0200

    version [0.0.220]

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,1 +1,4 @@
-export { LayoutWithoutSidebar, LayoutWithoutSidebarProps } from "@layouts/layout-without-sidebar";
+export {
+  LayoutWithoutSidebar as LayoutWithoutSidebar,
+  LayoutWithoutSidebarProps as LayoutWithoutSidebarProps,
+} from "@layouts/layout-without-sidebar";
