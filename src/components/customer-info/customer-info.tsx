7ec36f37b4f9c02f88eb6a72f0500ced1838e5d4
arthur.zakharov <arthur.zakharov@legal.one>
2024-05-10 09:26:52 +0200

    version [0.0.237]

--- src/components/customer-info/customer-info.tsx
+++ src/components/customer-info/customer-info.tsx
@@ -13,6 +13,8 @@ export interface CustomerInfoProps {
 export const CustomerInfo: FC<CustomerInfoProps> = (props) => {
   const { version, list, isEditButtonVisible = false, onClick } = props;
 
+  console.log("list", list);
+
   const customerInfoCn = (): string => {
     return cn("customer-info", {
       "customer-info--sidebar": version === "sidebar",
@@ -35,9 +37,9 @@ export const CustomerInfo: FC<CustomerInfoProps> = (props) => {
           </button>
         ) : null}
       </div>
-      {list.map((listItem, index) => (
-        <p key={index}>{listItem}</p>
-      ))}
+      {/*{list.map((listItem, index) => (*/}
+      {/*  <p key={index}>{listItem}</p>*/}
+      {/*))}*/}
     </div>
   );
 };
