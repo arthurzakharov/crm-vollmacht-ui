de53fc56fbef5fa3fa3de4602d0e879638f9b57e
arthur.zakharov <arthur.zakharov@legal.one>
2024-05-16 17:49:27 +0200

    version [0.0.264]

--- src/form-elements/input/input.tsx
+++ src/form-elements/input/input.tsx
@@ -105,7 +105,7 @@ export const Input: FC<InputProps> = (props) => {
       className={inputCn()}
       disabled={disabled}
       mask="99/99/9999"
-      maskPlaceholder="TT/MM/JJJJ"
+      maskPlaceholder="__/__/____"
       placeholder={placeholder}
       onClick={onDateClick}
       onKeyDown={onDateKeyDown}
