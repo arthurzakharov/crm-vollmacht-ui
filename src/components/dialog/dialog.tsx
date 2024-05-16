4af2010063a7355ceeb7ec86bcd05cd89069ff73
arthur.zakharov <arthur.zakharov@legal.one>
2024-05-16 13:24:06 +0200

    version [0.0.261]

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -1,67 +1,70 @@
 import React, { useEffect, useState, useRef, Fragment, FC, ReactNode } from "react";
-import { useScrollLock, useOnClickOutside } from "usehooks-ts";
+import { useScrollLock, useOnClickOutside, useToggle } from "usehooks-ts";
 import cn from "classnames";
 import "./dialog.css";
 
-type DialogRecord = {
+export type DialogRecord = {
   name: string;
   element: ReactNode;
 };
 
+export type DialogPosition = "top" | "center";
+
+export type DialogSize = "s" | "m";
+
 export interface DialogProps {
   dialogs: DialogRecord[];
-  name: null | string;
-  position: null | string;
-  size: null | string;
+  lockTarget?: string;
+  name?: string;
+  position?: DialogPosition;
+  size?: DialogSize;
   onOverlayClick: () => void;
   onClose: () => void;
 }
 
 export const Dialog: FC<DialogProps> = (props) => {
-  const { isLocked, lock, unlock } = useScrollLock({ autoLock: false, lockTarget: "#root" });
-  const [isMounted, setIsMounted] = useState<boolean>(false);
-  const [isClosing, setIsClosing] = useState<boolean>(true);
+  const { dialogs, lockTarget = "#root", name = "", position = "top", size = "m", onOverlayClick, onClose } = props;
+  const { isLocked, lock, unlock } = useScrollLock({ autoLock: false, lockTarget });
+  const [isMounted, toggleIsMounted] = useToggle(false);
+  const [isClosing, toggleIsClosing] = useToggle(true);
   const [localDialog, setLocalDialog] = useState<string | null>(null);
   const contentRef = useRef<HTMLDivElement>(null);
 
-  const isDialogContentVisible = (dialogName: string): boolean => {
-    return props.name === dialogName || localDialog === dialogName;
-  };
+  const isDialogContentVisible = (dialogName: string): boolean => name === dialogName || localDialog === dialogName;
 
-  const dialogCn = (): string => {
-    return cn("dialog", props.name && !isClosing ? "dialog--visible" : "dialog--hidden", {
-      "dialog--top": props.position === "top",
-      "dialog--center": props.position === "center",
-      "dialog--s": props.size === "s",
-      "dialog--m": props.size === "m",
+  const dialogCn = (): string =>
+    cn("dialog", name && !isClosing ? "dialog--visible" : "dialog--hidden", {
+      "dialog--top": position === "top",
+      "dialog--center": position === "center",
+      "dialog--s": size === "s",
+      "dialog--m": size === "m",
     });
-  };
 
-  useOnClickOutside(contentRef, props.onOverlayClick);
+  useOnClickOutside(contentRef, onOverlayClick);
 
   useEffect(() => {
-    if (props.name) setLocalDialog(props.name);
-    if (props.name && !isMounted) {
-      setIsMounted(true);
-      setIsClosing(false);
+    if (name) setLocalDialog(name);
+    if (name && !isMounted) {
+      toggleIsMounted();
+      toggleIsClosing();
       isLocked ? unlock() : lock();
     }
-    if (!props.name && isMounted) {
+    if (!name && isMounted) {
       isLocked ? unlock() : lock();
       setTimeout(() => {
-        setIsMounted(false);
+        toggleIsMounted();
         setLocalDialog(null);
-        props.onClose();
+        onClose();
       }, 300);
     }
-  }, [props.name, isMounted]);
+  }, [name, isMounted]);
 
-  return (props.name || localDialog) && isMounted ? (
+  return (name || localDialog) && isMounted ? (
     <div className={dialogCn()}>
       <div ref={contentRef} className="dialog__content">
-        {props.dialogs.map((dialog: DialogRecord) => {
-          return isDialogContentVisible(dialog.name) ? <Fragment key={dialog.name}>{dialog.element}</Fragment> : null;
-        })}
+        {dialogs.map((dialog: DialogRecord) =>
+          isDialogContentVisible(dialog.name) ? <Fragment key={dialog.name}>{dialog.element}</Fragment> : null,
+        )}
       </div>
     </div>
   ) : null;
